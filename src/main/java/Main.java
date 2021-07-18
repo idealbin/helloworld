bb1abaa010f659c3e57f4ef4c92cf6627316aeca
anbin <idealbin@naver.com>
2021-07-18 21:29:42 +0900

    feat : initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+
+        System.out.println("a + b = " + c);
+
+        System.out.println("Hello World");
+    }
+}
